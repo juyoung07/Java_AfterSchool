29bf7c163cacffeb02b8389552b01c7864537eb5
juyoung07 <yunju5090@gmail.com>
2023-10-20 07:44:58 +0900

    08/03. 메소드 맛보기?

---
+++ Java0804/src/Java0803.java
@@ -0,0 +1,22 @@
+
+public class Java0803 {
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+
+	//main() 메소드에서 실행 시작
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,  10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+
+	}
+
+}
