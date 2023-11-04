650038eefedd0529ed6501e677423dc49fcbba17
juyoung07 <yunju5090@gmail.com>
2023-11-04 23:00:06 +0900

    10/17. title, author멤버 변수를 가진 클래스 모델링

---
+++ Java1017/src/Book.java
@@ -0,0 +1,20 @@
+// 클래스 (main()X)
+public class Book {
+	String title;
+	String author;
+	
+	// 생성자
+	public Book(){}
+	public Book(String title) {
+		this.title = title;
+		author = "작가미상";
+	}
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+//	// r객체
+//	public 
+	
+}
