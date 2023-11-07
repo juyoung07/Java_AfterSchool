b04266790bff4ebef2c3601a85a5438948d2cfb7
juyoung07 <yunju5090@gmail.com>
2023-11-07 09:09:53 +0900

    10/17. Student 객체 만들기

---
+++ Java1017/src/StudentMain.java
@@ -0,0 +1,14 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student st1 = new Student("윤주영", 1211, 15, "112");
+		System.out.println(st1.name + " " + st1.stuld + " " + st1.age + " " + st1.phone);
+		
+		Student st2 = new Student("윤주영", 1211, 16, "010-4893-6063");
+		System.out.println(st2.name + " " + st2.stuld + " " + st2.age + " " + st2.phone);
+		
+
+	}
+
+}
