70e0983ced77e65e322a2026afbec05b6359eb0b
juyoung07 <yunju5090@gmail.com>
2023-11-07 09:09:20 +0900

    10/17. 이름, 학번, 나이, 연락처의 멤버변수를 가진 Student 클래스 모델링

---
+++ Java1017/src/Student.java
@@ -0,0 +1,20 @@
+// 학생을 나타내는 클래스 Student를 만들기
+// 학생 이름, 학번, 나이, 연락처의 멤버변수를 가짐
+public class Student {
+	String name;
+	int stuld;
+	int age;
+	String phone;
+	
+	public Student(){}
+//	public Student(String name, int stuld) {
+//		this.name = name;
+//		this.stuld = stuld;
+//	}
+	public Student(String name, int stuld, int age, String phone) {
+		this.name = name;
+		this.stuld = stuld;
+		this.age = age;
+		this.phone = phone;
+	}
+}
