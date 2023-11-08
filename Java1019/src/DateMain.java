f7a539b001f36cd935bbe27d0f04aa81f801e39c
juyoung07 <yunju5090@gmail.com>
2023-11-09 08:29:14 +0900

    10/19. Date 객체 만들기

---
+++ Java1019/src/DateMain.java
@@ -0,0 +1,15 @@
+
+public class DateMain {
+
+	public static void main(String[] args) {
+		Date today = new Date(2023, 10, 19);
+		today.print1();
+		today.print2();
+		
+		Date date2 = new Date(2023, 7, 12);
+		date2.print1();
+		date2.print2();
+
+	}
+
+}
