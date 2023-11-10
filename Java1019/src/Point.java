82a3d1dc811ca055a454cec2f14c72ac4a585292
juyoung07 <yunju5090@gmail.com>
2023-11-10 23:41:59 +0900

    10/19. x, y 멤버 변수를 가진 클래스 모델링

---
+++ Java1019/src/Point.java
@@ -0,0 +1,13 @@
+
+public class Point {
+	int x, y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void print() {
+		System.out.println("x의 좌표: " + x + "\ny의 좌표: " + y + "\n");
+	}
+}
